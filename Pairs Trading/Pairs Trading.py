f419ad56df7dbdae640d118bf2e0c939be554659
Evian <evian720@yahoo.com.hk>
2020-04-22 18:15:34 +0800

    Fix the global variables and update the stock universe

--- Pairs Trading/Pairs Trading.py
+++ Pairs Trading/Pairs Trading.py
@@ -12,6 +12,7 @@ import datetime
 import math
 import os
 from itertools import combinations
+import threading
 
 import talib
 from talib import MA_Type
@@ -20,8 +21,7 @@ from talib import MA_Type
 import win32com.client as win32
 from win32com.client import Dispatch, constants
 
-from html.parser import HTMLParser
-
+import time
 
 import matplotlib.pyplot as plt
 start_date = '2015-01-01'
@@ -30,6 +30,7 @@ end_date = datetime.datetime.today().strftime("%Y-%m-%d")
 stock_universe_path= r'C:\Users\Evian Zhou\Documents\Python\Pairs Trading\Paris.csv'
 stock_hist_data_path = r'C:\Users\Evian Zhou\Documents\Python\Pairs Trading\Hist_data' + '\\'
 stock_output_path = r'C:\Users\Evian Zhou\Documents\Python\Pairs Trading\Out_put' + '\\' + end_date + '\\'
+
 
 # Create folder for output
 try:
@@ -41,9 +42,8 @@ else:
 
 
 
-
 # Define the rolling mean duration
-rolling_mean_duration = 210
+rolling_mean_duration = 420
 
 def listToString(s):  
     
@@ -108,8 +108,12 @@ def calculate_correlation(Stock_1_file, Stock_2_file, Source_File_Folder):
     
     # Comine two DataFrame
     pairs = pd.concat([stock_1_Close_price, stock_2_Close_price], axis=1)
-    pairs.columns = [stock_1_name + '_Close',stock_2_name + '_Close']
+    pairs.reset_index(inplace=True)
+    pairs.columns = ['Date', stock_1_name + '_Close',stock_2_name + '_Close']
+    pairs.set_index('Date', inplace=True)
     
+    # Drop NA column for those unmatched date
+    pairs.dropna(inplace=True)
     
     # Calculate the ratio
     pairs['Ratio'] = pairs[stock_1_name + '_Close'] / pairs[stock_2_name + '_Close']
@@ -126,7 +130,7 @@ def calculate_correlation(Stock_1_file, Stock_2_file, Source_File_Folder):
     # Check if break
     pairs.loc[(pairs['Ratio'] > pairs['Upper_band']), "Break_Upper"] = True
     pairs.loc[(pairs['Ratio'] < pairs['Lower_band']),  "Break_Lower"] = True
-    
+    #print(pairs)
     
     fig, axs = plt.subplots(2, 1, constrained_layout=True,figsize=(16, 12))
     pairs['Ratio'].plot(ax=axs[0], lw=2, label='Ratio')
@@ -150,18 +154,17 @@ def calculate_correlation(Stock_1_file, Stock_2_file, Source_File_Folder):
     fig.suptitle(stock_1_name + ' VS ' + stock_2_name + ' ' + end_date, fontsize=16)
     fig.savefig(stock_output_path + stock_1_name + ' VS ' + stock_2_name + ' ' + end_date + '.png')
     
+
     pairs.to_csv(stock_output_path + stock_1_name + '_' + stock_2_name + '_' + end_date + '.csv')
-    #print(pairs)
+
     
     
+def loop_thru_files_get_signal(target_date, output_df):
     
-def loop_thru_files_get_signal(target_date):
     target_sources = os.listdir(stock_output_path)
     target_sources = [s for s in target_sources if target_date in s and '.csv' in s and 'output' not in s]
     
-    global output_df
-    
-    #print(target_sources)
+    print("Looping thru all the pairs files and searching for signals...")
     
     for filename in target_sources: 
         filepath = stock_output_path + filename
@@ -171,6 +174,7 @@ def loop_thru_files_get_signal(target_date):
         
         pairs = pd.read_csv(filepath)
         
+
         # ONLY Check for the lastest record
         pairs = pairs.iloc[[-1]]
         
@@ -181,17 +185,22 @@ def loop_thru_files_get_signal(target_date):
         pairs = pairs[ (pairs['Break_Upper'].notna()) | (pairs['Break_Lower'].notna()) ]
         pairs.rename(columns={pairs.columns[1]: "Stock_1_Close", pairs.columns[2]: "Stock_2_Close"}, inplace = True)
         # Add the result to output dataframe
+        output_df = output_df.sort_values(by=['Pairs_Name'])
+        
         
         output_df = pd.concat([output_df, pairs], axis=0)
-            
-
+    
+    
+    print("Search signals completed!")
+    
+    return output_df
 
 
 def loop_thru_combinations(stock_universe_path):
     stock_universe = pd.read_csv(stock_universe_path)
     stock_universe = stock_universe.values.tolist()
     
-    global output_df
+    print("Looping thru all the combinations and generate pairs..")
     
     for i in stock_universe:
         combination_stock_list = i[0].split(',')
@@ -204,12 +213,15 @@ def loop_thru_combinations(stock_universe_path):
             stock_name_2_path = i[1] + '_' + end_date + '.csv'
             
             calculate_correlation(stock_name_1_path, stock_name_2_path, stock_hist_data_path)
-            
+    
+    print("Pairs data generated!")
             
 
 def draft_email(subject, recipients, dataframe, auto=False): 
+    
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
+
 
     if hasattr(recipients, 'strip'):
         recipients = [recipients]
@@ -238,7 +250,7 @@ def draft_email(subject, recipients, dataframe, auto=False):
     for pairs in dataframe['Pairs_Name']:
         pairs = pairs.replace(' ', ' VS ')
         pairs_path = stock_output_path + pairs + ' ' + end_date + '.png'
-        print(pairs_path)
+        
         mail.Attachments.Add(Source = pairs_path)
         
     
@@ -250,22 +262,25 @@ def draft_email(subject, recipients, dataframe, auto=False):
 
 def main():
     
-    global output_df
     output_df = pd.DataFrame(columns=['Date', 'Pairs_Name', 'Stock_1_Close', 'Stock_2_Close', 'Ratio', \
                                                'Ratio_Moving_Avg', 'Sigma', 'Upper_band', 'Lower_band', \
                                                    'Break_Upper', 'Break_Lower'])
     
     Download_stock_price(stock_universe_path, stock_hist_data_path)
 
-    loop_thru_files_get_signal(end_date)
-    
     loop_thru_combinations(stock_universe_path)
+
+    output_df = loop_thru_files_get_signal(end_date, output_df)
+    
     
     # Save output file
     output_df.to_csv(stock_output_path +'output_' + end_date + '.csv')
     
+
     # Draft email
     draft_email('Paris ratio ' + end_date, 'investment@optimascap.com', output_df,  auto=False)
+    
+    
 if __name__ == '__main__':
 
         main()
